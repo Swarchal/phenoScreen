9cc82e0b6f7a4eed851bc5c56f9c35b54a8941e3
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-02-16 17:17:15 +0000

    add plotting functionality

--- QC/cv_check.R
+++ QC/cv_check.R
@@ -10,11 +10,24 @@
 ################################################################
 
 cv_check <- function(data, group){
+    
+    # to calculate CV
+    CV <- function(data){
+        (sd(data, na.rm = TRUE)/mean(data, na.RM = TRUE))*100
+    }
+    
+    # calculate CV for given data and groups
+    cv_df <- aggregate(data, list(group = group), CV)
+    names(cv_df)[2] <- "CV"
 
-	require(raster)
-
-	cv_df <- aggregate(data, list(group = group), cv)
-	names(cv_df)[2] <- "CV"
-	return(cv_df)
+    # try and get plots working: *not currently working*
+    plt <- plot(cv_df[,1],
+                cv_df[,2],
+                type = "b",
+                ylim = c(0, max(cv_df[,2])),
+                xlab = "Group",
+                ylab = "CV",
+                main = "CV Plot")
+    print(plt)
+    return(cv_df)
 }
-
