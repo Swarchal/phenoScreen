1fdefe0c4365e6df500941734871e82a61d05277
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-03-06 12:19:37 +0000

    add load_csv

---
+++ load_files.R
@@ -0,0 +1,9 @@
+load_files <- function(path){
+    
+    temp = list.files(path, pattern="*.csv")
+    
+    for (i in 1:length(temp)){
+        assign(temp[i], read.csv(temp[i]), envir = .GlobalEnv)
+    }  
+    
+}
