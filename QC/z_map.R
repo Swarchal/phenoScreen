81f1cd7603d316348c5b05bb70d170bce7f06b27
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-04-15 15:41:28 +0100

    add argument for 384-well plates

--- QC/z_map.R
+++ QC/z_map.R
@@ -11,7 +11,7 @@
 #   - platemap; vector of well identifiers, eg. "A01", "B12" etc.
 ###############################################################################
 
-z_map <- function(values, platemap, title = "", palette = "Spectral"){
+z_map <- function(values, platemap, plate = 96, title = "", palette = "Spectral"){
     
     require(ggplot2)
     require(dplyr)
@@ -32,8 +32,8 @@ z_map <- function(values, platemap, title = "", palette = "Spectral"){
     # RColorBrewerPallette
     my_cols <- brewer.pal(3, palette)
     
-    # produce a plate map in ggplot
-    plt <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+    # produce a plate map in ggplot (96-well format)
+    plt_96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
         geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
                    color = "grey90", fill = "white", shape = 21, size = 6) +
         geom_point(aes(fill = scaled_data), colour = "gray20", shape = 21, size = 10) +
@@ -46,5 +46,22 @@ z_map <- function(values, platemap, title = "", palette = "Spectral"){
                              mid = my_cols[2]) +
         ggtitle(title)+
         theme_bw()
-    return(plt)
+    
+    # produce a plate map in ggplot (384-well format)
+    plt_384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+        geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
+                   color = "grey90", fill = "white", shape = 22, size = 3) +
+        geom_point(aes(fill = scaled_data), colour = "gray20", shape = 22, size = 5) +
+        coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+        scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
+        scale_x_continuous(breaks = seq(1, 24)) +
+        scale_fill_gradient2("z-score",
+                             low = my_cols[3],
+                             high = my_cols[1],
+                             mid = my_cols[2]) +
+        ggtitle(title)+
+        theme_bw()
+    
+    if (plate == 96)  {return(plt_96)}
+    if (plate == 384) {return(plt_384)}
 }
