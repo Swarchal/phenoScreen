fc12b2f13bfa207b4ad9e4ad945991dfdc63362a
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-06-19 11:22:17 +0100

    make arguments consistent, add error handling for incorrect plate dimensions

--- R/z_map.R
+++ R/z_map.R
@@ -1,18 +1,20 @@
-z_map <-
-function(values, platemap, plate = 96, title = "", palette = "Spectral"){
+z_map <- function(data, well,
+    plate = 96,
+    title = "",
+    palette = "Spectral"){
     
     require(ggplot2)
     require(dplyr)
     require(RColorBrewer)
     
     # transform well labels into row-column values for a 96-well plate
-    platemap <- as.data.frame(platemap)
+    platemap <- as.data.frame(well)
     names(platemap)[1] <- "well"
     platemap <- mutate(platemap,
                        Row = as.numeric(match(toupper(substr(well, 1, 1)), LETTERS)),
                        Column = as.numeric(substr(well, 2, 5)))
     
-    values <- as.data.frame(values)
+    values <- as.data.frame(data)
     scaled_data <- scale(values)
     platemap <- cbind(platemap, scaled_data[,1])
     names(platemap)[4] <- "scaled_data"
@@ -49,7 +51,12 @@ function(values, platemap, plate = 96, title = "", palette = "Spectral"){
                              mid = my_cols[2]) +
         ggtitle(title) +
         theme_bw()
-    
+
+    if (length(well) > plate) {
+        stop("Invalid plate selection. The data given has more rows than number of wells. \nAre you sure argument 'plate' is correct for the number of wells in your data? \nnote: Default is set to a 96-well plate.",
+            call. = FALSE)
+    }
     if (plate == 96)  {return(plt_96)}
-    if (plate == 384) {return(plt_384)}
+    if (plate == 384) {return(plt_384)
+    } else stop("Not a valid plate format. Enter either 96 or 384.", call. = FALSE)
 }
