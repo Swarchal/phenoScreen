8c4f3a5a97449eeede8741efdcb2a09448f1b0d8
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-06-19 11:23:30 +0100

    consistent arguments. Add 384-well plate option and error handling for wrong dimensions.

--- R/pc_map.R
+++ R/pc_map.R
@@ -1,8 +1,10 @@
-pc_map <-
-function(data, well, title = "", palette = "Spectral"){
+pc_map <- function(data, well,
+    plate = 96,
+    title = "",
+    palette = "Spectral"){
     
-    pca_data <- princomp(data) # pca of data
-    pc1 <- pca_data$score[,1] # take first principal component
+    pca_data <- prcomp(data) # pca of data
+    pc1 <- pca_data$x[,1] # take first principal component
     
     z_map <- function(values, platemap, title = "", palette = "Spectral"){
         
@@ -26,7 +28,7 @@ function(data, well, title = "", palette = "Spectral"){
         my_cols <- brewer.pal(3, palette)
         
         # produce a plate map in ggplot
-        plt <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+        plt_96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
             geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
                        color = "grey90", fill = "white", shape = 21, size = 6) +
             geom_point(aes(fill = scaled_data), colour = "gray20", shape = 21, size = 10) +
@@ -39,7 +41,28 @@ function(data, well, title = "", palette = "Spectral"){
                                  mid = my_cols[2]) +
             ggtitle(title)+
             theme_bw()
-        return(plt)
+        
+        plt_384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+            geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
+                       color = "grey90", fill = "white", shape = 22, size = 3) +
+            geom_point(aes(fill = scaled_data), colour = "gray20", shape = 22, size = 5) +
+            coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+            scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
+            scale_x_continuous(breaks = seq(1, 24)) +
+            scale_fill_gradient2("z-score",
+                                 low = my_cols[3],
+                                 high = my_cols[1],
+                                 mid = my_cols[2]) +
+            ggtitle(title) +
+            theme_bw()
+
+        if (length(well) > plate) {
+            stop("Invalid plate selection. The data given has more rows than number of wells \nAre you sure argument 'plate' is correct for the number of wells in your data? \nnote: Default is set to a 96-well plate.",
+                call. = FALSE)
+        }
+        if (plate == 96)  {return(plt_96)}
+        if (plate == 384) {return(plt_384)
+        } else stop("Not a valid plate format. Enter either 96 or 384.", call. = FALSE)
     }
     
     plot_pc_map <- z_map(pc1, well, title, palette)
