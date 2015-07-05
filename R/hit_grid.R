49540158ba5f01e96b97d1c49dc8fd85a92b6b57
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-07-05 15:34:01 +0100

    initial commit

---
+++ R/hit_grid.R
@@ -0,0 +1,104 @@
+hit_grid <- function(data, well,
+                   plate_id,
+                   threshold = 2,
+                   ncols = 2,
+                   plate = 96,
+                   title = "",
+                   scale = "all",
+                   palette = "Spectral"){
+    
+    ## multiple platemap plots in a single figure using facet_wrap
+    
+    require(dplyr)
+    require(ggplot2)
+    require(RColorBrewer)
+    
+    ## transform well labels into row-column values for a 96-well plate
+    ## need to include plate_id labels into this dataframe
+    ## scale for all values, or scale per plate??
+    
+    
+    if (scale == "all"){
+        
+        # normalised across entire range of values
+        platemap <- as.data.frame(well)
+        names(platemap)[1] <- "well"
+        platemap <- mutate(platemap,
+                           Row = as.numeric(match(toupper(substr(well, 1, 1)), LETTERS)),
+                           Column = as.numeric(substr(well, 2, 5)))
+        values <- as.data.frame(data)
+        plate_label <- as.data.frame(plate_id)
+        scaled_data <- scale(values)
+        platemap <- cbind(platemap, scaled_data[,1], plate_id)
+        names(platemap)[4] <- "scaled_data"
+        names(platemap)[5] <- "plate_label"
+        platemap$hit <- NA
+        
+    } else if (scale == "plate"){
+        stop("Not added this yet.")
+        #         ## need to add scale-by-plate functionality        
+        #         # normalised on a plate-by-plate basis
+        #         platemap <- as.data.frame(well)
+        #         names(platemap)[1] <- "well"
+        #         platemap <- mutate(platemap,
+        #                            Row = as.numeric(match(toupper(substr(well, 1, 1)), LETTERS)),
+        #                            Column = as.numeric(substr(well, 2, 5)))
+        #         values <- as.data.frame(data)
+        #         plate_label <- as.data.frame(plate_id)
+        #         scaled_data <- values # just raw/un-scaled values for now
+        #         platemap <- cbind(platemap, scaled_data[,1], plate_id)
+        #         names(platemap)[4] <- "scaled_data"
+        #         names(platemap)[5] <- "plate_label"
+        #         platemap_temp <- group_by(platemap, plate_label)
+        #         platemap_temp2 <- summarise(platemap_temp, scale(scaled_data))
+        #         platemap <- as.data.frame(platemap_temp2)
+        
+        
+    } else stop("Not a valid input for 'scale'. \nOptions: 'all' or 'plate'.")
+    
+    
+    # calculate whether values are beyond the threshold; defined as hit or null
+    for (row in 1:nrow(platemap)){
+        if (scaled_data[row] > threshold){platemap$hit[row] <- "hit"
+        } else  if (scaled_data[row] < (-1 * threshold)){platemap$hit[row] <- "neg_hit"
+        } else {platemap$hit[row] <- "null"}
+    }
+    
+    # RColorBrewerPallette
+    my_cols <- brewer.pal(3, palette)
+    my_colours <- c(hit = my_cols[1], neg_hit = my_cols[3], null = my_cols[2])
+    
+    # produce a 96-well plate map layout in ggplot
+    plt_96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+        geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
+                   color = "grey90", fill = "white", shape = 21, size = 6) +
+        geom_point(aes(fill = hit), colour = "gray20", shape = 21, size = 10) +
+        coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
+        scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
+        scale_x_continuous(breaks = seq(1, 12)) +
+        ggtitle(title) + 
+        scale_fill_manual("hit", values = my_colours) + 
+        theme_bw() + 
+        facet_wrap(~plate_label,
+                   ncol = ncols)
+    
+    # produce a 384-well plate map layout in ggplot
+    plt_384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+        geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
+                   color = "grey90", fill = "white", shape = 22, size = 3) +
+        geom_point(aes(fill = hit), colour = "gray20", shape = 22, size = 5) +
+        coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+        scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
+        scale_x_continuous(breaks = seq(1, 24)) +
+        ggtitle(title) +
+        scale_fill_manual("hit", values = my_colours) + 
+        theme_bw() +
+        facet_wrap(~plate_label,
+                   ncol = ncols)
+    
+    if (plate == 96)  {return(plt_96)}
+    if (plate == 384) {return(plt_384)
+    } else stop("Not a valid plate format. Enter either 96 or 384.", call. = FALSE)
+    
+    
+}
