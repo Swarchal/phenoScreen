e39990c59dbbc10a429c1585a3c9753a6dc2d7d9
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-07-06 21:01:33 +0100

    initial commit

---
+++ R/pc_grid.R
@@ -0,0 +1,83 @@
+pc_grid <- function(data, well,
+                    plate_id,
+                    ncols = 2,
+                    plate = 96,
+                    title = "",
+                    palette = "Spectral"){
+  
+  pca_data <- prcomp(data) # pca of data
+  pc1 <- pca_data$x[, 1] # take first principal component
+  
+  # define z_map with multiple plots via facet_wrap
+  z_map <- function(data, well,
+                    plate,
+                    plate_id,
+                    ncols,
+                    title = "",
+                    palette = "Spectral"){
+    
+    require(ggplot2)
+    require(dplyr)
+    require(RColorBrewer)
+    
+    # transform well labels into row-column values for a 96-well plate
+    platemap <- as.data.frame(well)
+    names(platemap)[1] <- "well"
+    platemap <- mutate(platemap,
+                       Row = as.numeric(match(toupper(substr(well, 1, 1)), LETTERS)),
+                       Column = as.numeric(substr(well, 2, 5)))
+    values <- as.data.frame(pc1)
+    plate_label <- as.data.frame(plate_id)
+    scaled_data <- scale(values)
+    platemap <- cbind(platemap, scaled_data[,1], plate_id)
+    names(platemap)[4] <- "scaled_data"
+    names(platemap)[5] <- "plate_label"
+    
+    # RColorBrewerPallette
+    my_cols <- brewer.pal(3, palette)
+    
+    # produce a plate map in ggplot
+    if (plate == 96){
+      plt <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+        geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
+                   color = "grey90", fill = "white", shape = 21, size = 6) +
+        geom_point(aes(fill = scaled_data), colour = "gray20", shape = 21, size = 10) +
+        coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
+        scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
+        scale_x_continuous(breaks = seq(1, 12)) +
+        scale_fill_gradient2("z-score",
+                             low  = my_cols[3],
+                             mid  = my_cols[2],
+                             high = my_cols[1]) +
+        ggtitle(title) +
+        theme_bw() +
+        facet_wrap(~plate_id,
+                   ncol = ncols)
+    }
+    
+    if (plate == 384){
+      plt <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+        geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
+                   color = "grey90", fill = "white", shape = 22, size = 3) +
+        geom_point(aes(fill = scaled_data), colour = "gray20", shape = 22, size = 5) +
+        coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+        scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
+        scale_x_continuous(breaks = seq(1, 24)) +
+        scale_fill_gradient2("z-score",
+                             low  = my_cols[3],
+                             mid  = my_cols[2],
+                             high = my_cols[1]) +
+        ggtitle(title) +
+        theme_bw() + 
+        facet_wrap(~plate_id,
+                   ncol = ncols)
+    } else stop("Not a valid argument for plate. \nOptions: 96 or 384",
+                call. = FALSE)
+    
+    return(plt)
+  }
+  
+  plot_pc_map <- z_map(pc1, well, title, palette)
+  return(plot_pc_map)
+  
+}
