286b244450f91839789ff1a2612f056db12a88b7
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-07-14 19:10:13 +0100

    increase facet spacing, remove uneccessary variable

--- R/pc_grid.R
+++ R/pc_grid.R
@@ -1,9 +1,9 @@
 pc_grid <- function(data, well,
-                     plate_id,
-                     ncols = 2,
-                     plate = 96,
-                     title = "",
-                     palette = "Spectral"){
+                    plate_id,
+                    ncols = 2,
+                    plate = 96,
+                    title = "",
+                    palette = "Spectral"){
   
   pca_data <- prcomp(data) # pca of data
   pc1 <- pca_data$x[, 1] # take first principal component
@@ -25,43 +25,49 @@ pc_grid <- function(data, well,
   
   # RColorBrewerPallette
   my_cols <- brewer.pal(3, palette)
+  if (plate == 96){  
+    # produce a plate map in ggplot
+    plt_96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+      geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
+                 color = "grey90", fill = "white", shape = 21, size = 6) +
+      geom_point(aes(fill = scaled_data), colour = "gray20", shape = 21, size = 10) +
+      coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
+      scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
+      scale_x_continuous(breaks = seq(1, 12)) +
+      scale_fill_gradient2("z-score",
+                           low  = my_cols[3],
+                           mid  = my_cols[2],
+                           high = my_cols[1]) +
+      ggtitle(title) +
+      theme_bw() +
+      theme(panel.margin.x = unit(1, "lines"), 
+            panel.margin.y = unit(1, "lines")) + # increase spacing between facets
+      facet_wrap(~plate_label,
+                 ncol = ncols)
+    
+    return(plt_96)
+  }
   
-  # produce a plate map in ggplot
-  plt96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
-    geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
-               color = "grey90", fill = "white", shape = 21, size = 6) +
-    geom_point(aes(fill = scaled_data), colour = "gray20", shape = 21, size = 10) +
-    coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
-    scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
-    scale_x_continuous(breaks = seq(1, 12)) +
-    scale_fill_gradient2("z-score",
-                         low  = my_cols[3],
-                         mid  = my_cols[2],
-                         high = my_cols[1]) +
-    ggtitle(title) +
-    theme_bw() +
-    facet_wrap(~plate_label,
-               ncol = ncols)
-  
-  plt384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
-    geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
-               color = "grey90", fill = "white", shape = 22, size = 3) +
-    geom_point(aes(fill = scaled_data), colour = "gray20", shape = 22, size = 5) +
-    coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
-    scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
-    scale_x_continuous(breaks = seq(1, 24)) +
-    scale_fill_gradient2("z-score",
-                         low  = my_cols[3],
-                         mid  = my_cols[2],
-                         high = my_cols[1]) +
-    ggtitle(title) +
-    theme_bw() + 
-    facet_wrap(~plate_label,
-               ncol = ncols)
-  
-  if (plate == 96)  {return(plt96)}
-  if (plate == 384) {return(plt384)}
+  if (plate == 384){
+    plt_384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+      geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
+                 color = "grey90", fill = "white", shape = 22, size = 3) +
+      geom_point(aes(fill = scaled_data), colour = "gray20", shape = 22, size = 5) +
+      coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+      scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
+      scale_x_continuous(breaks = seq(1, 24)) +
+      scale_fill_gradient2("z-score",
+                           low  = my_cols[3],
+                           mid  = my_cols[2],
+                           high = my_cols[1]) +
+      ggtitle(title) +
+      theme_bw() +
+      theme(panel.margin.x = unit(1, "lines"), 
+            panel.margin.y = unit(1, "lines")) + # increase spacing between facets
+      facet_wrap(~plate_label,
+                 ncol = ncols)
+    
+    return(plt_384)
+  }
   else stop("Invalid argument for plate. \nOption: 96 or 384.")
 }
-
-
