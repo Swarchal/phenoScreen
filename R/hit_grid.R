78204eba7b3fa9676a1fa201b14f929bf59ccba9
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-07-15 13:52:33 +0100

    Update hit_grid.R
    - add grid spacings
    - remove unnecessary plt variable with plate argument

--- R/hit_grid.R
+++ R/hit_grid.R
@@ -69,37 +69,47 @@ hit_grid <- function(data, well,
     my_cols <- brewer.pal(3, palette)
     my_colours <- c(hit = my_cols[1], neg_hit = my_cols[3], null = my_cols[2])
     
-    # produce a 96-well plate map layout in ggplot
-    plt_96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
-        geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
-                   color = "grey90", fill = "white", shape = 21, size = 6) +
-        geom_point(aes(fill = hit), colour = "gray20", shape = 21, size = 10) +
-        coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
-        scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
-        scale_x_continuous(breaks = seq(1, 12)) +
-        ggtitle(title) + 
-        scale_fill_manual("hit", values = my_colours) + 
-        theme_bw() + 
-        facet_wrap(~plate_label,
-                   ncol = ncols)
     
-    # produce a 384-well plate map layout in ggplot
-    plt_384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
-        geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
-                   color = "grey90", fill = "white", shape = 22, size = 3) +
-        geom_point(aes(fill = hit), colour = "gray20", shape = 22, size = 5) +
-        coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
-        scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
-        scale_x_continuous(breaks = seq(1, 24)) +
-        ggtitle(title) +
-        scale_fill_manual("hit", values = my_colours) + 
-        theme_bw() +
-        facet_wrap(~plate_label,
-                   ncol = ncols)
-    
-    if (plate == 96)  {return(plt_96)}
-    if (plate == 384) {return(plt_384)
-    } else stop("Not a valid plate format. Enter either 96 or 384.", call. = FALSE)
-    
-    
+    if (plate == 96){
+      # produce a 96-well plate map layout in ggplot
+      plt_96 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+          geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
+                     color = "grey90", fill = "white", shape = 21, size = 6) +
+          geom_point(aes(fill = hit), colour = "gray20", shape = 21, size = 10) +
+          coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
+          scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
+          scale_x_continuous(breaks = seq(1, 12)) +
+          ggtitle(title) + 
+          scale_fill_manual("hit", values = my_colours) + 
+          theme_bw() +
+          theme(panel.margin.x = unit(1, "lines"), 
+          panel.margin.y = unit(0.5, "lines")) + # increase spacing between facets
+          facet_wrap(~plate_label,
+                     ncol = ncols)
+      
+      return(plt_96)
+      }
+      
+    if (plate == 384){
+      # produce a 384-well plate map layout in ggplot
+      plt_384 <- ggplot(data = platemap, aes(x = Column, y = Row)) +
+          geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
+                     color = "grey90", fill = "white", shape = 22, size = 3) +
+          geom_point(aes(fill = hit), colour = "gray20", shape = 22, size = 5) +
+          coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
+          scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
+          scale_x_continuous(breaks = seq(1, 24)) +
+          ggtitle(title) +
+          scale_fill_manual("hit", values = my_colours) + 
+          theme_bw() +
+          theme(panel.margin.x = unit(1, "lines"), 
+          panel.margin.y = unit(0.5, "lines")) + # increase spacing between facets
+          facet_wrap(~plate_label,
+                     ncol = ncols)
+                     
+      return(plt_384)
+    }
+  
+  else stop("Not a valid plate format. Enter either 96 or 384.", call. = FALSE)
+  
 }
