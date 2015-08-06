df59bace44533e098273fb30b636e5fc65e02ca5
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-08-06 17:54:37 +0100

    consistent layout to plot_confidence

--- R/area_of_confidence.R
+++ R/area_of_confidence.R
@@ -14,10 +14,13 @@ area_of_confidence <- function(x, y, confidence = 0.9){
     }
     
     # select quantile associated with confidence interval
-    confidencebound <- quantile(pp, (1 - confidence), na.rm = TRUE)
+    confidencebound <- quantile(pp,
+                                (1 - confidence),
+                                na.rm = TRUE)
     
     # produce a contour line from KDE and confidence quantiles
-    out_line <- contourLines(kerneld, levels = confidencebound)
+    out_line <- contourLines(kerneld,
+                             levels = confidencebound)
     OL_x <- out_line[[1]]$x
     OL_y <- out_line[[1]]$y
     
