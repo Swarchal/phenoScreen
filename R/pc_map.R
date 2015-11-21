0fb6342c7f80e2bf9bb708dfed72af0f4bd644aa
Swarchal <S.Warchal@sms.ed.ac.uk>
2015-11-21 21:03:35 +0000

    add missing plate argument

--- R/pc_map.R
+++ R/pc_map.R
@@ -6,7 +6,7 @@ pc_map <- function(data, well,
     pca_data <- prcomp(data) # pca of data
     pc1 <- pca_data$x[,1] # take first principal component
     
-    plot_pc_map <- z_map(pc1, well, title, palette)
+    plot_pc_map <- z_map(pc1, well, plate, title, palette)
     return(plot_pc_map)
      
 }
