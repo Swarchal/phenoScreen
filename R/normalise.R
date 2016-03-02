658f155c6cb07c696257b7b02919f2d968ce6eee
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-02 16:26:49 +0000

    add normalisation functions

---
+++ R/normalise.R
@@ -0,0 +1,39 @@
+#' Get featuredata columns
+#'
+#' Identifies columns in a dataframe that are not labelled Metadata
+#'
+#' @param x dataframe
+#' @export
+
+get_featuredata <- function(x){
+    setdiff(1:ncol(x), grep("Metadata_", colnames(x)))
+}
+
+
+#' Normalise per plate against negative control
+#'
+#' This function will normalise multivariate data on a plate-by-plate basis
+#' by dividing each feature by the median of the negative control for
+#' that feature. Note: Assumes any metadata columns are prefixed with
+#' "Metadata_", and everything else is numerical featuredata
+#'
+#' @param df dataframe
+#' @param plate_id string, name of the column of plate names, name for each plate
+#' @param compound string, name of column of compound names
+#' @param neg_compound, name of the negative control to normalise against
+#'
+#' @import dplyr
+#' @export
+
+normalise <- function(df, plate_id, compound = "Metadata_compound", neg_compound = "DMSO"){
+    stopifnot(is.data.frame(df))
+    
+    # identify feature data columns
+    feature_data <- get_featuredata(df)
+
+    out <- df %>%
+	group_by_(plate_id) %>%
+	mutate_each_(funs(./median(.[compound == neg_compound], na.rm = TRUE)), feature_data)
+	return(out)
+}
+
