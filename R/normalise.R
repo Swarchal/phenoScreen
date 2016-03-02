22cd8304cd9770a368831169b6ac4e9854c0f1fe
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-02 16:47:38 +0000

    add feature scaling functions

--- R/normalise.R
+++ R/normalise.R
@@ -37,3 +37,15 @@ normalise <- function(df, plate_id, compound = "Metadata_compound", neg_compound
 	return(out)
 }
 
+#' Scale feature data
+#'
+#' Z-score of feature data, each features scaled separately
+#'
+#' @param df dataframe
+#' @export
+
+scale_features <- function(df){
+    feature_data <- get_feature_data(df)
+    apply(df[, feature_data], 2, scale)
+}
+
