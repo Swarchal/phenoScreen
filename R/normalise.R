e279b78608cc47fa0a617f256e94bf73818f409f
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-14 12:31:58 +0000

    variable metadata pattern

--- R/normalise.R
+++ R/normalise.R
@@ -3,6 +3,7 @@
 #' Identifies columns in a dataframe that are not labelled Metadata
 #'
 #' @param x dataframe
+#' @param metadata_prefix pattern to match metadata columns
 #' @export
 
 get_featuredata <- function(x, metadata_prefix = "Metadata"){
