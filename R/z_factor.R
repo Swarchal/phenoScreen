d8ec2ee407b05e61a25a1bfb84f691363686c670
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-14 16:59:30 +0000

    use abs() rather than if statement

--- R/z_factor.R
+++ R/z_factor.R
@@ -27,16 +27,9 @@ z_factor <- function(positive, negative){
   mu_p <- mean(positive, na.rm = TRUE)
   mu_n <- mean(negative, na.rm = TRUE)    
 
-      # idiot proof error handling for input order
-      # assigns input with greatest mean as upper group, smaller mean -> lower group
-    if (mu_n > mu_p){
-      mu_p <- mean(negative, na.rm = TRUE)
-      mu_n <- mean(positive, na.rm = TRUE)
-    }
-
   sigma_p <- sd(positive)
   sigma_n <- sd(negative)
   
-  z_prime <- 1 - ((3*(sigma_p + sigma_n)) / (mu_p - mu_n))
+  z_prime <- 1 - ((3*(sigma_p + sigma_n)) / abs(mu_p - mu_n))
   return(z_prime)
 }
