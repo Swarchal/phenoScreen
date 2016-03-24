38cf245cc1d7ac77a26439c382b242c4532cc44e
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-24 12:54:35 +0000

    test scale_features

---
+++ tests/testthat/test-scale_features.R
@@ -0,0 +1,23 @@
+context("scale_features")
+
+# example data
+v1 <- rnorm(96, 10, 10)
+v2 <- rnorm(96, 100, 100)
+meta <- sample(letters, 96, replace = TRUE)
+df <- data.frame(Metadata_x = meta,
+		 v1, v2)
+out <- scale_features(df)
+
+test_that("scale_features returns a dataframe",{
+    expect_is(out, 'data.frame')
+})
+
+test_that("scale features returns correct dimensions",{
+    expect_equal(nrow(out), 96L)
+    expect_equal(ncol(out), 2L) # only returns scaled columns
+})
+
+test_that("scales values",{
+   expect_equal(mean(out$v1), 0L, tolerance = 1e-3)
+   expect_equal(mean(out$v2), 0L, tolerance = 1e-3)
+})
