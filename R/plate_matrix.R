1dda5d5e1de67b518e1698fac4e1df6c03467d14
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-03-28 15:11:39 +0100

    add plate matrix and rotate matrix functions

---
+++ R/plate_matrix.R
@@ -0,0 +1,48 @@
+#' plate layout matrix from well IDs
+#'
+#' Given a dataframe of alpha-numeric well IDs e.g ("A01"), and values,
+#' this function will produce a matrix in the form of a plate layout.
+#'
+#' @param data vector of data to be placed in matrix
+#' @param well vector of alphanumeric well IDs. e.g ("A01")
+#' 
+#' @return matrix
+#' 
+#' @export
+#' 
+#' @examples 
+#' a <- 1:96
+#' wells <- num_to_well(1:96)
+#' plate_matrix(data = a, well = wells)
+#' 
+#' x <- rnorm(384)
+#' wells <- num_to_well(1:384, plate = 384)
+#' plate_matrix(data = x, well = wells, plate = 384)
+
+
+plate_matrix <- function(data, well, plate = 96){
+    
+    platemap <- plate_map(data, well)
+    
+    # fill plate incase of missing values
+    filled_plate <- fill_plate(df = platemap, well = "well")
+    
+    # create empty matrix
+    if (plate == 96L) {
+        mat <- matrix(NA, nrow = 8, ncol = 12)
+    } else if (plate == 384L){
+        mat <- matrix(NA, nrow = 16, ncol = 24)
+    } else {
+        stop("incorrect plate size -- 96 or 384 only")
+    }
+    
+    # fill mat with values (header) from Row and Column index
+    for (i in 1:nrow(filled_plate)){
+        row <- filled_plate[i, 'Row']
+        col <- filled_plate[i, 'Column']
+        val <- filled_plate[i, 'values']
+        mat[row, col] <- val
+    }
+    
+    return(mat)
+}
