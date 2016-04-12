aa9cc880f0755bbacc38327c9fe9835517123a4c
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-04-12 15:23:46 +0100

    add 1536 plate option

--- R/num_to_well.R
+++ R/num_to_well.R
@@ -1,7 +1,7 @@
 #' Converts numbers to well labels
 #' 
 #' Converts numerical values to corresponding alpha-numeric well labels
-#' for either 96 or 384 well plates.
+#' for 96, 384 or 1536 well plates.
 #' 
 #' @param x Vector of numbers to be converted
 #' @param plate Number of wells in complete plate (96 or 384)
@@ -22,12 +22,21 @@ num_to_well <- function(x, plate = 96){
     stopifnot(is.numeric(x))
 
     if (plate == 96L){
-      rows <- LETTERS[1:8]
-      columns <- 1:12
+	rows <- LETTERS[1:8]
+	columns <- 1:12
     } else if (plate == 384L){
-      rows <- LETTERS[1:16]
-      columns <- 1:24 
-    } else stop("Plate needs to be 96 or 384")
+	rows <- LETTERS[1:16]
+	columns <- 1:24 
+    } else if (plate == 1536L){
+	first_26 <- LETTERS[1:26]
+	last_6 <- vector(length = 6)
+	for (i in 1:6){
+	    last_6[i] <- paste(LETTERS[rep(i, 2)],
+		    collapse = "")
+	}
+	rows <- c(first_26, last_6)
+	columns <- 1:48
+    } else stop("Plate needs to be 96, 384 or 1536")
     
     # columns then rows for normal row-wise counting  
     combinations <-  expand.grid(columns, rows)
