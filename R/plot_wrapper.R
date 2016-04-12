22be654f889351a0c3aef121728ffc8031c5de6e
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-04-12 17:52:45 +0100

    export 1536 platemap

--- R/plot_wrapper.R
+++ R/plot_wrapper.R
@@ -35,3 +35,23 @@ plt384 <- function(platemap){
         scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
         scale_x_continuous(breaks = seq(1, 24))
 }
+
+#' ggplot plate object
+#'
+#' internal function
+#' @param platemap platemap dataframe produced by \code{plate_map}
+#' @return ggplot object
+#' @import ggplot2
+#' @export
+
+
+plt1536 <- function(platemap){
+    ggplot(data = platemap, aes(x = Column, y = Row)) +
+	geom_point(data = expand.grid(seq(1, 48), seq(1, 32)), aes(x = Var1, y = Var2),
+		   color = "grey90", fill = "white", shape = 22, size = 2) + 
+	geom_point(aes(fill = values), colour = "gray20", shape = 22, size = 3.5) + 
+	coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(0.5, 32.5)) + 
+	scale_y_reverse(breaks = seq(1, 32)) +
+	scale_x_continuous(breaks = seq(1, 48)) + 
+	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
+}
