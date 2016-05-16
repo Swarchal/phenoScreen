a710dff70e367a871ff12b05e2e64dd0d459de44
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-05-16 11:23:48 +0100

    test for increased normality

--- tests/testthat/test-transformations.R
+++ tests/testthat/test-transformations.R
@@ -1,4 +1,5 @@
 context("transformations")
+set.seed(12321)
 
 test_that("returns numbers", {
     x <- rnorm(1000)
@@ -12,4 +13,16 @@ test_that("returns a matrix", {
     mat_out <- glog(mat)
     expect_true(is.matrix(mat_out))
     expect_equal(ncol(mat_out), 100)
+})
+
+test_that("roughly normalises data", {
+    # create a skewed distribution
+    a <- rnorm(1000)^2
+    # w value from shapiro wilk test, high = normal distribution
+    w_a <- shapiro.test(a)$statistic
+    # glog transformation and w value
+    glog_a <- glog(a)
+    w_glog_a <- shapiro.test(glog_a)$statistic
+    # check transformation if more normal than original data
+    expect_true(w_a < w_glog_a)
 })
