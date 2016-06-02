ccae4a7e02728cf75cd112bf925c9c24c74cee8f
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-06-02 15:47:57 +0100

    error checking at start of function

--- R/raw_map.R
+++ R/raw_map.R
@@ -1,13 +1,13 @@
 #' Plots a platemap with heatmap of raw values
-#' 
+#'
 #' Converts numerical values and  well labels into multiple plate heatmaps
-#' 
+#'
 #' @param data Numerical values to be plotted
 #' @param well Vector of well identifiers e.g "A01"
 #' @param plate Number of wells in complete plate (96, 384 or 1536)
 #' @param title Title of plot
 #' @param palette RColorBrewer palette
-#' 
+#'
 #' @import ggplot2
 #' @import dplyr
 #' @import RColorBrewer
@@ -15,11 +15,11 @@
 #' @return ggplot plot
 #'
 #' @export
-#' 
+#'
 #' @examples
 #' df <- data.frame(vals = rnorm(1:384),
 #'   well = num_to_well(1:384, plate = 384))
-#' 
+#'
 #' raw_map(data = df$vals,
 #'         well = df$well,
 #'         title = "Title of plot")
@@ -28,14 +28,18 @@ raw_map <- function(data, well,
                     plate = 96,
                     title = "",
                     palette = "YlGnBu"){
-  
+
     if (!is.vector(data)){
 	stop("'data' has to be a single column or a vector")
     }
-  
+
+    if (length(well) > plate) {
+        stop("Invalid plate selection. The data given has more rows than the number of wells. \nAre you sure argument 'plate' is correct for the number of wells in your data? \nnote: Default is set to a 96-well plate.")
+    }
+    
     # transform well labels into row-column values
     platemap <- plate_map(data, well)
-  
+
     if (plate == 96){
 	plt <- plt96(platemap) +
 	    scale_fill_distiller("values", palette = palette) +
@@ -47,9 +51,9 @@ raw_map <- function(data, well,
 	    ggtitle(title) +
 	    theme_bw()
     } else if (plate == 1536L){
-	plt <- plt1536(platemap) + 
-	    scale_fill_distiller("values", palette = palette) + 
-	    ggtitle(title) + 
+	plt <- plt1536(platemap) +
+	    scale_fill_distiller("values", palette = palette) +
+	    ggtitle(title) +
 	    theme_bw()
     } else{
 	stop("Invalid argument for 'plate'. \nOption: 96, 384 or 1546",
@@ -57,5 +61,5 @@ raw_map <- function(data, well,
     }
 
     return(plt)
-  
+
 }
--- R/z_map.R
+++ R/z_map.R
@@ -1,13 +1,13 @@
 #' Plots a platemap with heatmap of scaled values
-#' 
+#'
 #' Converts numerical values and  well labels into multiple plate heatmaps
-#' 
+#'
 #' @param data Numerical values to be plotted
 #' @param well Vector of well identifiers e.g "A01"
 #' @param plate Number of wells in complete plate (96, 384 or 1536))
 #' @param title Title of plot
 #' @param palette RColorBrewer palette
-#' 
+#'
 #' @return ggplot plot
 #'
 #' @import ggplot2
@@ -18,7 +18,7 @@
 #' @examples
 #' df <- data.frame(vals = rnorm(1:384),
 #'   well = num_to_well(1:384, plate = 384))
-#' 
+#'
 #' z_map(data = df$vals,
 #'       well = df$well,
 #'       plate = 384,
@@ -31,16 +31,22 @@ z_map <- function(data, well,
     palette = "Spectral"){
 
     stopifnot(is.vector(data))
-    
+
+
+    if (length(well) > plate) {
+        stop("Invalid plate selection. The data given has more rows than number of wells. \nAre you sure argument 'plate' is correct for the number of wells in your data? \nnote: Default is set to a 96-well plate.",
+            call. = FALSE)
+    }
+
     platemap <- plate_map_scale(data, well)
-    
+
     if (plate == 96L){
         # produce a plate map in ggplot (96-well format)
         plt <- plt96(platemap) +
             scale_fill_distiller("z-score", palette = palette) +
             ggtitle(title) +
             theme_bw()
-           
+
     } else if (plate == 384L){
         # produce a plate map in ggplot (384-well format)
         plt <- plt384(platemap) +
@@ -49,19 +55,14 @@ z_map <- function(data, well,
             theme_bw()
 
     } else if (plate == 1536L) {
-	plt <- plt1536(platemap) + 
+	plt <- plt1536(platemap) +
 	    scale_fill_distiller("z-score", palette = palette) +
-	    ggtitle(title) + 
+	    ggtitle(title) +
 	    theme_bw()
 
     } else stop("Not a valid plate format. Enter either 96, 384 or 1536.",
 		call. = FALSE)
 
-    if (length(well) > plate) {
-        stop("Invalid plate selection. The data given has more rows than number of wells. \nAre you sure argument 'plate' is correct for the number of wells in your data? \nnote: Default is set to a 96-well plate.",
-            call. = FALSE)
-    } 
 
     return(plt)
 }
-
