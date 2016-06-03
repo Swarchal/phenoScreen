ab3d73632e9b610fa7b11afe55bfb90cf2051fb8
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-06-03 11:35:19 +0100

    Fix issue of empty wells with theme_dark

--- R/plot_wrapper.R
+++ R/plot_wrapper.R
@@ -1,7 +1,7 @@
 #' ggplot plate object
-#' 
+#'
 #' internal function
-#' 
+#'
 #' @param platemap platemap dataframe produced by \code{plate_map}
 #' @return ggplot object
 #' @import ggplot2
@@ -10,7 +10,7 @@
 plt96 <- function(platemap){
     ggplot(data = platemap, aes(x = Column, y = Row)) +
         geom_point(data = expand.grid(seq(1, 12), seq(1, 8)), aes(x = Var1, y = Var2),
-                   color = "grey90", fill = "white", shape = 21, size = 6) +
+                   color = "grey90", fill = "white", shape = 21, size = 6, alpha = 0.1) +
         geom_point(aes(fill = values), colour = "gray20", shape = 21, size = 10) +
         coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
         scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
@@ -18,9 +18,9 @@ plt96 <- function(platemap){
 }
 
 #' ggplot plate object
-#' 
+#'
 #' internal function
-#' 
+#'
 #' @param platemap platemap dataframe produced by \code{plate_map}
 #' @return ggplot object
 #' @import ggplot2
@@ -29,7 +29,7 @@ plt96 <- function(platemap){
 plt384 <- function(platemap){
     ggplot(data = platemap, aes(x = Column, y = Row)) +
         geom_point(data = expand.grid(seq(1, 24), seq(1, 16)), aes(x = Var1, y = Var2),
-                   color = "grey90", fill = "white", shape = 22, size = 3) +
+                   color = "grey90", fill = "white", shape = 22, size = 3, alpha = 0.1) +
         geom_point(aes(fill = values), colour = "gray20", shape = 22, size = 5) +
         coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
         scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
@@ -48,10 +48,10 @@ plt384 <- function(platemap){
 plt1536 <- function(platemap){
     ggplot(data = platemap, aes(x = Column, y = Row)) +
 	geom_point(data = expand.grid(seq(1, 48), seq(1, 32)), aes(x = Var1, y = Var2),
-		   color = "grey90", fill = "white", shape = 22, size = 2) + 
-	geom_point(aes(fill = values), colour = "gray20", shape = 22, size = 3.5) + 
-	coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(0.5, 32.5)) + 
+		   color = "grey90", fill = "white", shape = 22, size = 2, alpha = 0.1) + 
+	geom_point(aes(fill = values), colour = "gray20", shape = 22, size = 3.5) +
+	coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(0.5, 32.5)) +
 	scale_y_reverse(breaks = seq(1, 32)) +
-	scale_x_continuous(breaks = seq(1, 48)) + 
+	scale_x_continuous(breaks = seq(1, 48)) +
 	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
 }
