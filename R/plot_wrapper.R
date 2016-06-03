ae205e87fca984b86cc668d68d9f2886de12f28e
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-06-03 12:00:45 +0100

    remove axis labels by default

--- R/plot_wrapper.R
+++ R/plot_wrapper.R
@@ -14,7 +14,9 @@ plt96 <- function(platemap){
         geom_point(aes(fill = values), colour = "gray20", shape = 21, size = 10) +
         coord_fixed(ratio = (13 / 12) / (9 / 8), xlim = c(0.5, 12.5), ylim = c(0.5, 8.5)) +
         scale_y_reverse(breaks = seq(1, 8), labels = LETTERS[1:8]) +
-        scale_x_continuous(breaks = seq(1, 12))
+        scale_x_continuous(breaks = seq(1, 12)) +
+        xlab("") +
+        ylab("")
 }
 
 #' ggplot plate object
@@ -33,7 +35,9 @@ plt384 <- function(platemap){
         geom_point(aes(fill = values), colour = "gray20", shape = 22, size = 5) +
         coord_fixed(ratio = (24.5 / 24) / (16.5 / 16), xlim = c(0.5, 24.5), ylim = c(0.5, 16.5)) +
         scale_y_reverse(breaks = seq(1, 16), labels = LETTERS[1:16]) +
-        scale_x_continuous(breaks = seq(1, 24))
+        scale_x_continuous(breaks = seq(1, 24)) +
+        xlab("") +
+        ylab("")
 }
 
 #' ggplot plate object
@@ -48,10 +52,12 @@ plt384 <- function(platemap){
 plt1536 <- function(platemap){
     ggplot(data = platemap, aes(x = Column, y = Row)) +
 	geom_point(data = expand.grid(seq(1, 48), seq(1, 32)), aes(x = Var1, y = Var2),
-		   color = "grey90", fill = "white", shape = 22, size = 2, alpha = 0.1) + 
+		   color = "grey90", fill = "white", shape = 22, size = 2, alpha = 0.1) +
 	geom_point(aes(fill = values), colour = "gray20", shape = 22, size = 3.5) +
 	coord_fixed(ratio = (48.25 / 48) / (32.25 / 32), xlim = c(0.5, 48.5), ylim = c(0.5, 32.5)) +
 	scale_y_reverse(breaks = seq(1, 32)) +
 	scale_x_continuous(breaks = seq(1, 48)) +
-	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25))
+	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.25)) +
+    xlab("") +
+    ylab("")
 }
