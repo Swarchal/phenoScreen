519d4962f652f74eb7d8692b3b280290d7e8ecb2
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-09-13 12:04:31 +0100

    add pca function

--- R/transformations.R
+++ R/transformations.R
@@ -12,3 +12,42 @@ glog <- function(x, lambda = 0) {
     x <- as.matrix(x)
     log(0.5 * (x + sqrt(x^2 + lambda^2)))
 }
+
+
+#' principal component analysis
+#'
+#' replace feature columns with principal components
+#'
+#' @param data dataframe
+#' @param metadata_prefix string, prefix of metadata columns
+#' @param n_components integer, number of principal components to return
+#'      if left blank then will return all principal components which is
+#'      equal to the number of feature columns
+#' @param ...
+#'
+#' @importFrom stats prcomp
+#' @export
+pca <- function(data, metadata_prefix = "Metadata_", n_components = NULL, ...) {
+
+
+    feature_cols = get_feature_cols(data, metadata_prefix)
+    metadata_cols = get_metadata_cols(data, metadata_prefix)
+    metadata = data[, metadata_cols]
+    
+    # if n_components is not specified then return all principal components
+    if (is.null(n_components)) {
+        n_components = length(feature_cols)
+    }
+    
+    if (n_components > length(feature_cols)) {
+        stop("n_components > number of feature columns", call. = FALSE)
+    }
+
+    pc_comps = prcomp(data[, feature_cols], ...)$x[, 1:n_components]
+
+    # create column names for the principal component dataframe
+    colnames(pc_comps) = sprintf("PC%d", 1:n_components)
+
+    # merge principal components and metadata 
+    cbind(pc_comps, metadata)
+}
--- tests/testthat/test-transformations.R
+++ tests/testthat/test-transformations.R
@@ -1,21 +1,21 @@
 context("transformations")
 set.seed(12321)
 
-test_that("returns numbers", {
+test_that("glog returns numbers", {
     x <- rnorm(1000)
     out <- glog(x)
     expect_equal(length(x), length(out))
     expect_true(is.numeric(out))
 })
 
-test_that("returns a matrix", {
+test_that("glog returns a matrix", {
     mat <- matrix(rnorm(10000), ncol = 100)
     mat_out <- glog(mat)
     expect_true(is.matrix(mat_out))
     expect_equal(ncol(mat_out), 100)
 })
 
-test_that("roughly normalises data", {
+test_that("glog roughly normalises data", {
     # create a skewed distribution
     a <- rnorm(1000)^2
     # w value from shapiro wilk test, high = normal distribution
@@ -26,3 +26,44 @@ test_that("roughly normalises data", {
     # check transformation if more normal than original data
     expect_true(w_a < w_glog_a)
 })
+
+
+
+# create example data
+N_PLATES = 5
+wells = rep(platetools::num_to_well(1:96), N_PLATES)
+plate_id = rep(c("plate_1", "plate_2", "plate_3", "plate_4", "plate_5"),
+               each = 96)
+val1 = rnorm(96 * N_PLATES, 10, 10)
+val2 = rnorm(96 * N_PLATES, 1, 100)
+comps = c(rep("cmpd", 80), rep("DMSO", 16))
+compound = rep(comps, N_PLATES)
+
+df = data.frame(Metadata_well = wells,
+         Metadata_plate_id = plate_id,
+         Metadata_compound = compound,
+         val1, val2)
+
+out = pca(df)
+
+test_that("pca returns a dataframe", {
+    expect_is(out, "data.frame")
+})
+
+
+#test_that("pca retains metadata", {
+#    orig_metadata_columns = #TODO:
+#    pca_metadata_columns = #TODO: 
+#    expect_equal(orig_metadata_columns, pca_metadata_columns)
+#})
+
+
+#test_that("pca n_components works", {
+#    out_new = pca(df, n_components=1L)
+#    # TODO:
+#})
+
+
+#test_that("pca errors if n_components > num features", {
+#    # TODO:
+#})
